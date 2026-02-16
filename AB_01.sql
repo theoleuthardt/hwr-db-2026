13b0918aa5e08bb0f0a9280fee928240360650ed
theoleuthardt <theoleuthardt12@gmail.com>
2026-02-16 16:16:30 +0100

    init

---
+++ AB_01.sql
@@ -0,0 +1,15 @@
+/*
+    Autor:   Theo Leuthardt
+    MatrNr.: XXXXXXXXXXX
+	Einführung
+	Vereinfachung der Beziehungen
+*/
+
+/* ********* Beispiel einer einfachen DB ************* */
+
+drop table Z;
+create table Z
+(
+    Stern VARCHAR2(16),
+    Masse INTEGER
+);
